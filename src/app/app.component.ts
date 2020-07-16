ef5c155e4e65da5408e453bc042eb9b0034232ac
mahsanghani <ghaniahsan@outlook.com>
2020-07-16 08:33:41 -0400

    finished e-commerce angular application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {
-  title = 'angularpractice';
+export class AppComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { CustomersModule } from './customers/customers.module';
+import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {OrdersModule} from './orders/orders.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, CustomersModule, OrdersModule, SharedModule, CoreModule, AppRoutingModule ],
+  declarations: [ AppComponent, ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
